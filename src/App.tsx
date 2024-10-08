9a5ed2e6d49486458e979f74e9f2a4543b3fe152
cong0912 <congntc912@gmail.com>
2024-10-08 16:10:38 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,61 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoute, RouteType } from '@routes/routes';
-import MainLayout from '@layouts/MainLayout';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoute, RouteType } from "@routes/routes";
+import MainLayout from "@layouts/MainLayout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthLayout from "@layouts/AuthLayout";
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    {publicRoute.map((route: RouteType, index: number) => {
-                        const Page = route.component;
-                        let Layout = MainLayout;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {publicRoute.map((route: RouteType, index: number) => {
+            const Page = route.component;
+            let Layout = MainLayout;
+            if (route.layout === null) {
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <>
+                      <Page />
+                    </>
+                  }
+                />
+              );
+            } else if (route.layout === AuthLayout) {
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <AuthLayout>
+                      <Page />
+                    </AuthLayout>
+                  }
+                />
+              );
+            }
 
-                        if (route.layout === null) {
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <>
-                                            <Page />
-                                        </>
-                                    }
-                                />
-                            );
-                        }
-
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </Router>
-            <ToastContainer />
-        </div>
-    );
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
