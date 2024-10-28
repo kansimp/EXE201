6130fa188dffe6fc08bacdf477f31a5d19ccf66c
cong0912 <congntc912@gmail.com>
2024-10-28 14:21:50 +0700

    add

--- src/components/pages/Seller/TableProductsSeller.tsx
+++ src/components/pages/Seller/TableProductsSeller.tsx
@@ -250,6 +250,7 @@ export default function TableProductsSeller() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
+  console.log("so luong", products);
 
   return (
     <Box sx={{ height: 400, width: "100%" }}>
@@ -315,8 +316,8 @@ export default function TableProductsSeller() {
                   </label>
                   <input
                     type="number"
-                    name="quantity"
-                    id="quantity"
+                    name="stock"
+                    id="stock"
                     value={formData.stock}
                     onChange={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
